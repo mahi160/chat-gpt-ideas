32edfabe850b5079ca512a7f1537d1f6e4abe39c
Salauddin Omar Sifat <omarsifat288@gmail.com>
2023-05-25 02:18:29 +0600

    Add temp converter

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
+#![allow(unused)]
 mod guessing_game;
+mod temp_converter;
 
 use crate::guessing_game::guessing_game;
+use crate::temp_converter::temp_converter;
 fn main() {
-    guessing_game();
+    // guessing_game();
+
+    temp_converter(30.4, 'C');
 }
