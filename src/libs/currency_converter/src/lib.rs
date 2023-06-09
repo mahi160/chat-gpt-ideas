bb7ac43b7499349a7029395088b93dadaeeca9c8
Salauddin Omar Sifat <omarsifat288@gmail.com>
2023-06-09 12:08:42 +0600

    finish currency converter

--- src/libs/currency_converter/src/lib.rs
+++ src/libs/currency_converter/src/lib.rs
@@ -1,19 +1,31 @@
 use dotenv::dotenv;
 use fetch;
+use std::io;
 
 pub fn run() {
     dotenv().ok();
     let api_key: String = std::env::var("EXCHANGE_RATE_CURRENCY_API").unwrap();
     let host: String = String::from("https://v6.exchangerate-api.com/v6");
 
-    let mut amount = 100;
-
-    let mut from = "USD";
-    let mut to = "BDT";
+    let amount: f64 = take_input("What is the amount?").trim().parse().unwrap();
+    let from: String = take_input("From?").trim().parse().unwrap();
+    let to: String = take_input("To?").trim().parse().unwrap();
 
     let url = format!("{}/{}/pair/{}/{}/{}", host, api_key, from, to, amount);
 
     let response = fetch::get(&url).unwrap();
     let result = response["conversion_result"].as_f64().unwrap();
-    println!("{:?}", result);
+    println!(
+        "As of today's Exchange Rate, {:.2} {} is {:.2} {}!",
+        amount, from, result, to
+    );
+}
+
+fn take_input(text: &str) -> String {
+    let mut var = String::new();
+    println!("{}", text);
+    io::stdin()
+        .read_line(&mut var)
+        .expect("Could not read data.");
+    var
 }
--- src/libs/guessing_game/src/lib.rs
+++ src/libs/guessing_game/src/lib.rs
@@ -1,52 +1,24 @@
-// pub fn add(left: usize, right: usize) -> usize {
-//     left + right
-// }
-
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-
-//     #[test]
-//     fn it_works() {
-//         let result = add(2, 2);
-//         assert_eq!(result, 4);
-//     }
-// }
-
 use rand::{self, Rng};
 use std::{cmp::Ordering, io};
 
 pub fn guessing_game() {
     let random_number: u32 = rand::thread_rng().gen_range(1..=100);
-
     let input = io::stdin();
     let mut count = 0;
 
     println!("Guess a number between 1 and 100. You will get 5 chances!");
 
-    // for i in 0..5 {
-    //     let mut guess = String::new();
-    //     println!("Guess #{}", i + 1);
-    //     input.read_line(&mut guess).expect("Failed to read line");
-    //     let guess: u32 = guess.trim().parse().unwrap();
-    //     match guess.cmp(&random_number) {
-    //         Ordering::Less => println!("Too Low!"),
-    //         Ordering::Equal => {
-    //             println!("You win!");
-    //             break;
-    //         }
-    //         Ordering::Greater => println!("Too High!"),
-    //     }
-    // }
-
     loop {
         let mut guess = String::new();
         println!("Guess: ");
-        count += 1;
 
         input.read_line(&mut guess).expect("Failed to read line");
         let guess: u32 = guess.trim().parse().unwrap();
 
+        if count > 5 {
+            println!("You Lose! The number is {}", random_number);
+            break;
+        }
         match guess.cmp(&random_number) {
             Ordering::Equal => {
                 println!("You win!");
@@ -55,6 +27,7 @@ pub fn guessing_game() {
             Ordering::Greater => println!("Too High!"),
             Ordering::Less => println!("Too Low!"),
         }
+        count += 1;
     }
     println!(
         "The number was {} & it took you {} guesses!",
